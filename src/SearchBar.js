aa945831d97bc16e503b83a0b5b2df30d0d84099
Marta Petrella <martollum@gmail.com>
2018-11-27 14:48:07 +0100

    amends

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -21,7 +21,6 @@ class SearchBar extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Name:
           <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
